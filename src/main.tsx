cdb6dbee577036bd7e98c0cf5f4aed6445109e4b
dieulinhhoang <dieuleen1211@gmail.com>
2026-05-06 15:50:02 +0700

    feat: increase table font size to 18px using Ant Design v5 ConfigProvider
    Wrap App component with ConfigProvider to set theme.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConfigProvider } from 'antd';
 import App from './App';
 import './App.css';
 
@@ -9,7 +10,20 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ConfigProvider
+        theme={{
+          components: {
+            Table: {
+              cellFontSize: 18,
+              cellFontSizeMD: 18,
+              cellFontSizeSM: 16,
+              headerFontSize: 18,
+            },
+          },
+        }}
+      >
+        <App />
+      </ConfigProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
