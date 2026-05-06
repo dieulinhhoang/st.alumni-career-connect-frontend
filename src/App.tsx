a7811974cf04e516ca30c98855a425f2cd122286
dieulinhhoang <dieuleen1211@gmail.com>
2026-05-06 14:49:16 +0700

    fix: add ConfigProvider with fontSize token to make Ant Design table/components use larger font

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-
 import './App.css'
 import routes from './routes/AppRouter';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ConfigProvider } from 'antd';
+import viVN from 'antd/locale/vi_VN';
 
-const router=createBrowserRouter(routes) // tao router tu danh sach cac route v6.4 
+const router = createBrowserRouter(routes);
 
 function App() {
- 
   return (
-    <>
-      <RouterProvider router={router}></RouterProvider>
-    </>
-  )
+    <ConfigProvider
+      locale={viVN}
+      theme={{
+        token: {
+          fontSize: 16,
+          fontSizeSM: 14,
+          fontSizeLG: 18,
+          fontSizeXL: 20,
+          fontSizeHeading1: 36,
+          fontSizeHeading2: 28,
+          fontSizeHeading3: 22,
+          fontSizeHeading4: 18,
+          fontSizeHeading5: 16,
+        },
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  );
 }
 
-export default App
+export default App;
