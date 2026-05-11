d7e4e63a4e6aa56d0b4d24733d87485e00ef8078
dieulinhhoang <dieuleen1211@gmail.com>
2026-05-11 12:48:51 +0700

    fix(routes): correct /admin/statistics route element in Suspense

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -51,7 +51,7 @@ const routes = [
                 path: '/admin/statistics',
                 element: (
                     <Suspense fallback={<Loader />}>
-                         element: <FormStatisticsDetailPage />,
+                                         <FormStatisticsDetailPage />,
                     </Suspense>
                 )
             },
