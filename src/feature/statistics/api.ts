d82d404576cd049522051d03e5e3436597a38404
dieulinhhoang <dieuleen1211@gmail.com>
2026-05-12 16:07:08 +0700

    fix(feature/statistics): change /v1.0/ to /api/v1/ for consistency with project API paths

--- src/feature/statistics/api.ts
+++ src/feature/statistics/api.ts
@@ -2,12 +2,12 @@ import { api } from '../../../libs/api';
 import type { FormOption, StatisticalQuestion, FormStatisticsDetail } from './types';
 
 export const getForms = async (): Promise<FormOption[]> => {
-  const { data } = await api.get('/v1.0/statistics/forms');
+  const { data } = await api.get('/api/v1/statistics/forms');
   return data;
 };
 
 export const getStatisticalQuestions = async (formId: number): Promise<StatisticalQuestion[]> => {
-  const { data } = await api.get(`/v1.0/statistics/questions/${formId}`);
+  const { data } = await api.get(`/api/v1/statistics/questions/${formId}`);
   return data;
 };
 
@@ -15,6 +15,6 @@ export const getFormStatisticsDetail = async (
   formId: number,
   questionId: string,
 ): Promise<FormStatisticsDetail | null> => {
-  const { data } = await api.get(`/v1.0/statistics/detail/${formId}/${questionId}`);
+  const { data } = await api.get(`/api/v1/statistics/detail/${formId}/${questionId}`);
   return data;
 };
